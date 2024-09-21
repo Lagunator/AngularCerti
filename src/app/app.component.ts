e71da91321cfcf20cdae765853340531de97fe37
Lagunator <jhosiasmauricio@gmail.com>
2024-09-20 21:04:47 -0400

    [20/09/24] Personas Only pares

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,7 @@ import { RouterOutlet } from '@angular/router';
 export class AppComponent {
   title = 'AngularCerti';
   sumNumber = 5;
+  evenPersons: number[] = [];
 
 constructor(){
   console.log('subtract', this.subtract(8,4))
@@ -22,5 +23,15 @@ public sum(num1: number, num2:number): number{
 
 private subtract(num1: number, num2: number):number{
   return num1 - num2
+}
+
+public getArray(): number[] {
+  const person: number[] = [1, 2, 3, 4, 5];
+  return person.filter(p => p % 2 === 0); // Filtra solo los números pares
+}
+
+// Se ejecuta al inicializar el componente
+ngOnInit(): void {
+  this.evenPersons = this.getArray(); // Asignar el resultado de getArray() a evenPersons
 }
 }
