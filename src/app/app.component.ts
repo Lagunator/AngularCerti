06dc747479a6497c833154b0c750216687295c12
Lagunator <jhosiasmauricio@gmail.com>
2024-09-20 20:34:11 -0400

    [20/09/2024] Sum public

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,4 +11,16 @@ import { RouterOutlet } from '@angular/router';
 export class AppComponent {
   title = 'AngularCerti';
   sumNumber = 5;
+
+constructor(){
+  console.log('subtract', this.subtract(8,4))
+}
+
+public sum(num1: number, num2:number): number{
+  return num1 + 2
+}
+
+private subtract(num1: number, num2: number):number{
+  return num1 - num2
+}
 }
