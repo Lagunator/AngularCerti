59f609c66ef22166e93ab81fe3b2de194805eede
¨Lagunator¨ <jhosisasmauricio@gmail.com>
2024-10-25 23:23:02 -0400

    [25-010-2024] guard CanActivate

--- src/app/student/classmate/classmate.component.ts
+++ src/app/student/classmate/classmate.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ExamService } from '../exam.service';
 
 @Component({
   selector: 'app-classmate',
@@ -8,5 +9,11 @@ import { Component } from '@angular/core';
   styleUrl: './classmate.component.css'
 })
 export class ClassmateComponent {
-
+  newScores:number[] = []
+  constructor(private _examService: ExamService) { 
+    this._examService.getScoresAsObservable().subscribe(scores => {
+      console.log('SCORES: ', scores)
+      this.newScores = scores
+    })
+  }
 }
